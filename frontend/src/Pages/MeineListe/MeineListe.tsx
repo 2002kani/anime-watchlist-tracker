fbe81a8b3c09c86e5f11b64f2c9810e8995e0c5a
02Kani <kanikaradag02@gmail.com>
2025-03-18 23:37:24 +0100

    ui updated

--- frontend/src/Pages/MeineListe/MeineListe.tsx
+++ frontend/src/Pages/MeineListe/MeineListe.tsx
@@ -10,49 +10,62 @@ const MeineListe = () => {
     const { isLoading, error, data } = useSWR("http://localhost:5002/cards", fetcher);
 
     return(
-        <div className="meine-liste">
+        <>
             <div className="navbar-buttons-container">
-                <NavbarButtons />
+                    <NavbarButtons />
             </div>
-
-            <div className="liste-container">
-                <h1> Meine Liste </h1>
-            </div>
-
-            <div className="filter-container">
-                <FilterButtons />
-            </div>
-
-            <div className="sammlung-container">
-            { error && <h3> Fehler beim Laden der Seite! </h3> }
-            { isLoading && <h3> Lade Ergebnisse... </h3>}
-
-            {data ? (
-                data.map((card: ({ mal_id: number; title: string; img_url: string; rank: number; score: number; synopsis: string; })) => (
-                    <div className="sammlung-card" key={card.mal_id}>
-                        <div className="left-side">
-                            <div className="card-updaten">
-                                {/* TODO: hier jeweils favorisieren und delete button hinzufügen */}
-                            </div>
-                            <img src={card.img_url} alt={card.title} className="sammlung-img" />
-                            <h3>{card.title}</h3>
-                        </div>
-                        <div className="right-side">
-                            <div className="details-top">
-                                { card.synopsis ? <p>{truncateDescription(card.synopsis, 20)}</p> : <p> Fehler beim Laden der Beschreibung! Es ist Keine Beschreibung vorhanden.</p> }
-                            </div>
-                            <div className="details-bottom">
-                                <p> Platzierung: <span>{card.rank}</span></p>
-                                <p> Score: <span>{card.score}</span> </p>
-                            </div>
-                        </div>
+            
+            <div className="meine-liste">
+                <div className="top-container">
+                    <div className="liste-container">
+                        <h1> Meine Liste </h1>
                     </div>
-                ))
-            ) : (
-                <h3> Du hast bisher noch keine Anime in deiner Sammlung.. </h3>
-            )}
+                    <div className="filter-container">
+                        <FilterButtons />
+                    </div>
+                </div>
+
+                <div className="sammlung-container">
+                    { error && <h3> Fehler beim Laden der Seite! </h3> }
+                    { isLoading && <h3> Lade Ergebnisse... </h3>}
+                <ul>
+                    {data ? (
+                        data.map((card: ({ mal_id: number; title: string; img_url: string; rank: number; score: number; synopsis: string; })) => (
+                        <li>
+                            <div className="sammlung-card" key={card.mal_id}>
+                                <div className="left-side">
+                                    <div className="card-updaten">
+                                        <button id="sammlung-fav-button">
+                                            <i className='bx bx-heart'/>
+                                        </button>
+                                        <button id="sammlung-save-button">
+                                            <i className='bx bxs-bookmark'/>
+                                        </button>
+                                    </div>
+                                    <img src={card.img_url} alt={card.title} className="sammlung-img" />
+                                    <h3>{card.title}</h3>
+                                </div>
+                                <div className="right-side">
+                                    <div className="details-top">
+                                        { card.synopsis ? <p>{truncateDescription(card.synopsis, 26)}</p> : <p> Fehler beim Laden der Beschreibung! Es ist Keine Beschreibung vorhanden.</p> }
+                                    </div>
+                                    <div className="details-bottom">
+                                        <p> Platzierung: <span>#{card.rank}</span></p>
+                                        <p> Score: <span>{card.score}</span> </p>
+                                    </div>
+                                </div>
+                                <div className="far-right-side">
+                                    <p id="top">Bewertung: </p>
+                                    <p id="bottom"> <span> 8 </span> / 10 <i className='bx bx-edit'></i> </p>
+                                </div>
+                            </div>
+                        </li>
+                        ))
+                    ) : ( <h3> Du hast bisher noch keine Anime in deiner Sammlung.. </h3> )}
+                </ul>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
