62512b48fbdbed58f5cad1b80bc34dec15053ee6
02Kani <kanikaradag02@gmail.com>
2025-03-19 18:19:40 +0100

    #

--- frontend/src/Components/NavbarButtons/NavbarButtons.tsx
+++ frontend/src/Components/NavbarButtons/NavbarButtons.tsx
@@ -1,18 +1,31 @@
 import { useNavigate } from "react-router-dom";
 import "./NavbarButtons.css"
-
+import { useLocation } from "react-router-dom";
 
 const NavbarButtons = () => {
 
     const navigate = useNavigate();
+    const location = useLocation();
     
     return(
         <div className="navbar-buttons">
-            <button onClick={() => navigate("/")}>Suche</button>
+            <button 
+            className={ location.pathname === "/" ? "active" : "" }
+            onClick={() => navigate("/")}>
+                Suche
+            </button>
                 <span className="separator"></span>
-            <button onClick={() => navigate("/Meine-Liste")}> Sammlung </button>
+            <button 
+            className={ location.pathname === "/Meine-Liste" ? "active" : "" }
+            onClick={() => navigate("/Meine-Liste")}>
+                Sammlung
+            </button>
                 <span className="separator"></span>
-            <button onClick={() => navigate("/Favoriten")}>Favoriten</button>
+            <button
+            className={ location.pathname === "/Favoriten" ? "active" : "" }
+            onClick={() => navigate("/Favoriten")}>
+                Favoriten
+            </button>
         </div>
     );
 }
