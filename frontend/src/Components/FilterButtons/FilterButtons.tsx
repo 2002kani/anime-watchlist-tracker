c9cafc9dc54386bd5490105b75370464c9146118
02Kani <kanikaradag02@gmail.com>
2025-03-19 15:13:15 +0100

    #

--- frontend/src/Components/FilterButtons/FilterButtons.tsx
+++ frontend/src/Components/FilterButtons/FilterButtons.tsx
@@ -1,13 +1,30 @@
 import "./FilterButtons.css"
 import MiniSuchleiste from "../MiniSuchleiste/MiniSuchleiste";
+import { useState } from "react";
 
 const FilterButtons = () => {
+
+    const [activeFilter, setActiveFilter] = useState("neuste");
+
+    const activeFilterUpdate = (filter: string) => {
+        setActiveFilter(filter)
+    }
+
     return(
         <div className="filter-container">
             <div className="filter-left">
-                <button> Neusten </button>
-                <button> Ältesten </button>
-                <button> Favorisierte </button>
+                <button className={activeFilter === "neuste" ? "active" : ""}
+                        onClick={() => activeFilterUpdate("neuste")}>
+                    Neuste
+                </button>
+                <button className={activeFilter === "älteste" ? "active" : ""}
+                        onClick={() => activeFilterUpdate("älteste")}>
+                    Älteste
+                </button>
+                <button className={activeFilter === "favorisierte" ? "active" : ""}
+                        onClick={() => activeFilterUpdate("favorisierte")}>
+                    Favorisierte
+                </button>
             </div>
             <div className="filter-right">
                 <MiniSuchleiste />
